302ef75366b415124c76a105e2d91a6b4c5763b7
mazz <1216224856@qq.com>
2022-03-07 09:47:47 +0800

    add difference

--- learngit/test.cpp
+++ learngit/test.cpp
@@ -2,8 +2,10 @@
 int main() 
 {
 	using namespace std; 
-cout <<"Come up and C++ me some time.";    
-cout <<endl; 
-cout <<"You won't regret it!" << endl;/*cin.get();*/
-return 0; 
+	cout <<"Come up and C++ me some time.";    
+	cout <<endl; 
+	cout <<"You won't regret it!" << endl;/*cin.get();*/
+	cout << "git test code" << endl;
+	return 0; 
+
 }
