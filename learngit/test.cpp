119e37acb71a6cb15550d27f9a293630effe18a6
mazz <1216224856@qq.com>
2022-03-09 18:12:12 +0800

    删除前三行

--- learngit/test.cpp
+++ learngit/test.cpp
@@ -2,9 +2,6 @@
 int main() 
 {
 	using namespace std; 
-	cout <<"Come up and C++ me some time.";    
-	cout <<endl; 
-	cout <<"You won't regret it!" << endl;/*cin.get();*/
 	cout << "git test code" << endl;
 	cout << "branch" << endl;
 	cout << "and dev" << endl;
